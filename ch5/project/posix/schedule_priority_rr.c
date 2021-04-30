0bf705bb0e56cf08fe6ab6aaef1805a061130390
Fred Chan <fred2167@gmail.com>
2021-04-29 20:15:32 -0700

    priority_cc init

---
+++ ch5/project/posix/schedule_priority_rr.c
@@ -0,0 +1,15 @@
+#define MIN_PRIORITY 1
+#define MAX_PRIORITY 10
+
+#include "task.h"
+#include <stdio.h>
+
+// add a task to the list 
+void add(char *name, int priority, int burst){
+
+}
+
+// invoke the scheduler
+void schedule(){
+    
+}
